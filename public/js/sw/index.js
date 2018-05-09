15c84738d4c74630d7283ae6cf78858dd1593edb
Isaac Newton <isaacnewtonfx@gmail.com>
2018-05-09 06:46:36 +0000

    completed task-custom-response

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,11 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  
+	event.respondWith(
+		new Response("<span class='a-winner-is-me'>Isaac Newton</span> <b>Here!!!</b>",{
+			headers: {'Content-type':'text/Html'}
+		})
+	);
+
 });
